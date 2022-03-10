c6069f1fe94d5938285be440bbf65938b4fd5301
Shabnam790 <98453738+Shabnam790@users.noreply.github.com>
2022-03-10 19:58:14 +0530

    Add files via upload

---
+++ Terraform/Dev.tf
@@ -0,0 +1,5 @@
+variable "dev_var"{
+    description =" dev variable"
+    default ="rg_shabnam_dev"
+
+}
---
+++ Terraform/main.tf
@@ -0,0 +1,75 @@
+terraform{
+    required_providers{
+        azurerm={
+            source="hashicorp/azurerm"
+            version="2.98.0"
+        }
+    }
+}
+provider "azurerm"{
+    features{}
+}
+
+resource "azurerm_resource_group" "rg_shabnam_dev"{
+    name=var.dev_var
+    location="central india"
+}
+
+resource "azurerm_resource_group" "rg_Shabnam_qa" {
+  name     = var.qa_var
+  location = "Central India"
+}
+
+resource "azurerm_app_service_plan" "dev_plan_appservice" {
+  name                = "dev_plan_appservice"
+  location            = azurerm_resource_group.rg_shabnam_dev.location
+  resource_group_name = azurerm_resource_group.rg_shabnam_dev.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service_plan" "qa_plan_appservice" {
+  name                = "qa_plan_appservice"
+  location            = azurerm_resource_group.rg_Shabnam_qa.location
+  resource_group_name = azurerm_resource_group.rg_Shabnam_qa.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "dev-shabnam-appservice" {
+  name                = "dev-shabnam-appservice"
+  location            = azurerm_resource_group.rg_shabnam_dev.location
+  resource_group_name = azurerm_resource_group.rg_shabnam_dev.name
+  app_service_plan_id = azurerm_app_service_plan.dev_plan_appservice.id
+
+}
+
+resource "azurerm_app_service" "qa-Shabnam-appservice" {
+  name                = "qa-Shabnam-appservicee"
+  location            = azurerm_resource_group.rg_Shabnam_qa.location
+  resource_group_name = azurerm_resource_group.rg_Shabnam_qa.name
+  app_service_plan_id = azurerm_app_service_plan.qa_plan_appservice.id
+
+}
+
+resource "azurerm_storage_account" "sabnamdevstorageaccount" {
+  name                     = "sabnamdevstorageaccount"
+  resource_group_name      = azurerm_resource_group.rg_shabnam_dev.name
+  location                 = azurerm_resource_group.rg_shabnam_dev.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource "azurerm_storage_account" "sabnamqastorageaccount" {
+  name                     = "sabnamqastorageaccount"
+  resource_group_name      = azurerm_resource_group.rg_Shabnam_qa.name
+  location                 = azurerm_resource_group.rg_Shabnam_qa.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
